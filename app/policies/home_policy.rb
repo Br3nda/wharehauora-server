bb6388644db9d2dfe0d35f31fdbf57d467940059
Brenda Wallace <brenda@wallace.net.nz>
2016-09-25 21:27:42 +1300

    removed duplicate homes policy

--- app/policies/home_policy.rb
+++ app/policies/home_policy.rb
@@ -1,12 +1,4 @@
 class HomePolicy < ApplicationPolicy
   attr_reader :user, :home
 
-  def initialize(user, post)
-    @user = user
-    @post = post
-  end
-
-  # def update?
-  #   user.admin? or not post.published?
-  # end
 end
