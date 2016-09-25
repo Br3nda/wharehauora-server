9bc72c3a15b9b51fbf2c6aa6d0ac7e731468ec99
Brenda Wallace <brenda@wallace.net.nz>
2016-09-25 22:08:15 +1300

    RAYGUN_APIKEY

--- config/initializers/raygun.rb
+++ config/initializers/raygun.rb
@@ -2,8 +2,8 @@ if Rails.env.production?
   Raygun.setup do |config|
     config.filter_parameters = Rails.application.config.filter_parameters
 
-    if ENV['RAILS_SERVE_STATIC_FILES'].present?
-      config.api_key ENV['RAILS_SERVE_STATIC_FILES']
+    if ENV['RAYGUN_APIKEY'].present?
+      config.api_key = ENV['RAYGUN_APIKEY']
       config.enable_reporting = true
     else
       config.enable_reporting = false
