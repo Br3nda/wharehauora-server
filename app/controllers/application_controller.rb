df9de17be2d02c1d0667b0b7bc8989a5c8b2f1ca
Brenda Wallace <brenda@wallace.net.nz>
2016-09-25 18:22:46 +1300

    Base pundit policy to inherit from

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,20 @@
 # frozen_string_literal: true
 class ApplicationController < ActionController::Base
+  include Pundit
+
+  after_action :verify_authorized, except: :index
+  after_action :verify_policy_scoped, only: :index
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+  def user_not_authorized
+    flash[:error] = "You are not authorized to perform this action."
+    redirect_to(request.referer || root_path)
+  end
 end
