8f3e940160df562df2b38ce412eaf6fdf2c52ae6
Brenda Wallace <brenda@wallace.net.nz>
2016-09-25 19:49:27 +1300

    Readings model

---
+++ db/migrate/20160925063312_create_readings.rb
@@ -0,0 +1,10 @@
+class CreateReadings < ActiveRecord::Migration
+  def change
+    create_table :readings do |t|
+      t.references :sensor
+      t.text :key
+      t.float :value
+      t.timestamps null: false
+    end
+  end
+end
