da5d84bbffc75289ebdc6927585fff62eee59d2b
Brenda Wallace <brenda@wallace.net.nz>
2016-09-25 17:48:04 +1300

    only load raygun if production

--- config/initializers/raygun.rb
+++ config/initializers/raygun.rb
@@ -1,10 +1,12 @@
-Raygun.setup do |config|
-  config.filter_parameters = Rails.application.config.filter_parameters
+if Rails.env.production?
+  Raygun.setup do |config|
+    config.filter_parameters = Rails.application.config.filter_parameters
 
-  if ENV['RAILS_SERVE_STATIC_FILES'].present?
-    config.api_key ENV['RAILS_SERVE_STATIC_FILES']
-    config.enable_reporting = true
-  else
-    config.enable_reporting = false
+    if ENV['RAILS_SERVE_STATIC_FILES'].present?
+      config.api_key ENV['RAILS_SERVE_STATIC_FILES']
+      config.enable_reporting = true
+    else
+      config.enable_reporting = false
+    end
   end
 end
