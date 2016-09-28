a7841a75ed9566917a06313c5247239a07d5bc4b
Brenda Wallace <brenda@wallace.net.nz>
2016-09-28 03:12:08 +0000

    sensors linked directly to homes

--- app/models/sensor.rb
+++ app/models/sensor.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 class Sensor < ActiveRecord::Base
   belongs_to :room
+  belongs_to :home
   has_many :readings
 
-  delegate :home, :home_id, to: :room, allow_nil: false
+  # delegate :home, :home_id, to: :room, allow_nil: false
 end
