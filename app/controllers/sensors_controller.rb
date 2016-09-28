b92049a626112dd5f5f3335d331abdb99491b7fd
Brenda Wallace <brenda@wallace.net.nz>
2016-09-28 00:40:49 +0000

    Saving sensors against rooms

--- app/controllers/sensors_controller.rb
+++ app/controllers/sensors_controller.rb
@@ -15,4 +15,25 @@ class SensorsController < WebController
     authorize @room
     @sensor = Sensor.new(room_id: @room.id)
   end
+
+  def create
+    room = Room.find(params[:room_id])
+    authorize room
+    sensor = Sensor.new(sensor_params.merge(room_id: room.id))
+    sensor.save!
+    redirect_to room.home
+  end
+
+  private
+
+  def sensor_params
+    params[:sensor].permit(permitted_sensor_params)
+  end
+
+  def permitted_sensor_params
+    %i(
+      name
+      home_id
+    )
+  end
 end
