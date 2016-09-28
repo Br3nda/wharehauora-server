d170ae4ddf20c1c76f3d8159fa2e8f358e667625
Brenda Wallace <brenda@wallace.net.nz>
2016-09-28 00:37:32 +0000

    adding node_id to sensors

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160925063312) do
+ActiveRecord::Schema.define(version: 20160928003715) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20160925063312) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "node_id"
   end
 
   create_table "users", force: :cascade do |t|
