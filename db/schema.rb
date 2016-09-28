05d45006cf5ad9b5fbd92dedcf05436dac70d87b
Brenda Wallace <brenda@wallace.net.nz>
2016-09-28 03:03:37 +0000

    adding homeid to sensors, message type to readings
    because some sensors aren't assigned to rooms yet
    and readings can be temp or humid

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160928003715) do
+ActiveRecord::Schema.define(version: 20160928030123) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,8 +27,9 @@ ActiveRecord::Schema.define(version: 20160928003715) do
     t.integer  "sensor_id"
     t.text     "key"
     t.float    "value"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.string   "message_type"
   end
 
   create_table "rooms", force: :cascade do |t|
@@ -44,6 +45,7 @@ ActiveRecord::Schema.define(version: 20160928003715) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "node_id"
+    t.integer  "home_id"
   end
 
   create_table "users", force: :cascade do |t|
