54e58dac19f5190e9c442f98a3cc6d5cff966092
Brenda Wallace <brenda@wallace.net.nz>
2016-09-29 00:48:52 +0000

    tests for last reading on sensor model

--- spec/models/sensor_spec.rb
+++ spec/models/sensor_spec.rb
@@ -7,10 +7,12 @@ RSpec.describe Sensor, type: :model do
   let(:sensor) { FactoryGirl.create(:sensor) }
   context "no readings yet" do
     it { expect(sensor.temperature).to eq(nil) }
+    it { expect(sensor.last_reading).to eq(nil) }
   end
   context "readings have no values" do
     before { FactoryGirl.create(:reading, sensor: sensor, value: nil, sub_type: V_TEMP) }
     it { expect(sensor.temperature).to eq(nil) }
+    it { expect(sensor.last_reading).not_to eq(nil) }
   end
   context "lots of readings" do
     before do
@@ -18,5 +20,6 @@ RSpec.describe Sensor, type: :model do
       FactoryGirl.create(:reading, sensor: sensor, value: 10, sub_type: V_TEMP)
     end
     it { expect(sensor.temperature).to eq(10) }
+    it { expect(sensor.last_reading).not_to eq(nil) }
   end
 end
