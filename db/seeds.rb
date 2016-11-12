cd35f5d5056a35a0a0bc430d628f109e7225ee87
Brenda Wallace <brenda@wallace.net.nz>
2016-11-12 13:31:49 +1300

    Some seed home and sensor records

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,16 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-HomeType.create!(name: "State housing")
+state_house = HomeType.create!(name: "State housing")
 HomeType.create!(name: "Council")
 HomeType.create!(name: "Rental")
 HomeType.create!(name: "Owner occupier")
 
-RoomType.create!(name: "Living space")
-RoomType.create!(name: "Sleeping/Bedroom")
-RoomType.create!(name: "Kitchen")
+living_room = RoomType.create!(name: "Living space")
+bedroom = RoomType.create!(name: "Sleeping/Bedroom")
+
+home = Home.create!(name: "Example home 1", home_type: state_house)
+Sensor.create!(name: "living room", home: home, room_type_id: living_room.id)
+Sensor.create!(name: "parent's room", home: home, room_type_id: bedroom.id)
+Sensor.create!(name: "eldest child's room", home: home, room_type_id: bedroom.id)
+Sensor.create!(name: "youngest child's room", home: home, room_type_id: bedroom.id)
