6996a3258af40fcc1b447c998077730d7fd38796
Brenda Wallace <brenda@wallace.net.nz>
2016-12-10 19:28:27 +1300

    Specs for users #edit and #update, when not admin

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -7,14 +7,25 @@ RSpec.describe UsersController, type: :controller do
       it { expect(response).to redirect_to(user_session_path) }
     end
   end
+
   let(:user) { FactoryGirl.create(:user) }
+  let(:valid_params) { { email: 'bob@example.com' } }
   context 'user is signed in' do
     before { sign_in user }
     describe 'GET index' do
       before { get :index }
       it { expect(response).to redirect_to(root_path) }
     end
+    describe '#edit' do
+      before { get :edit, id: user.to_param }
+      it { expect(response).to redirect_to(root_path) }
+    end
+    describe '#update' do
+      before { patch :update, id: user.to_param, user: valid_params }
+      it { expect(response).to redirect_to(root_path) }
+    end
   end
+
   context 'user is admin' do
     let(:admin_role) { FactoryGirl.create(:role, name: 'janitor') }
     let(:admin) { FactoryGirl.create(:user, roles: [admin_role]) }
@@ -23,8 +34,6 @@ RSpec.describe UsersController, type: :controller do
       before { get :index }
       it { expect(response).to have_http_status(:success) }
     end
-
-    let(:valid_params) { { email: 'bob@example.com' } }
     describe '#edit' do
       before { get :edit, id: user.to_param }
       it { expect(response).to have_http_status(:success) }
