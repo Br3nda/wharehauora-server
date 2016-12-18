43d1c44f43759d72215c9671f251e8eb30b289be
Brenda Wallace <brenda@wallace.net.nz>
2016-12-18 20:47:43 +1300

    Removed un-used code

--- app/policies/application_policy.rb
+++ app/policies/application_policy.rb
@@ -6,35 +6,8 @@ class ApplicationPolicy
     @record = record
   end
 
-  def create?
-    owned_by_current_user?
-  end
-
-  def new?
-    user.present?
-  end
-
-  def edit?
-    owned_by_current_user?
-  end
-
-  def show?
-    owned_by_current_user? || current_user_authorised_to_view?
-  end
-
-  def update?
-    owned_by_current_user?
-  end
-
-  def destroy?
-    owned_by_current_user?
-  end
-
   class Scope
     attr_reader :user, :scope
-    def resolve
-      scope.where(owner_id: user.id)
-    end
 
     def initialize(user, scope)
       @user = user
@@ -43,10 +16,6 @@ class ApplicationPolicy
   end
 
   private
-
-  def owned_by_current_user?
-    record.owner_id == user.id
-  end
 
   def admin?
     user.present? && user.role?('janitor')
