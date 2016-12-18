704e3e4eb518da941291fa5d9375832022ef6c96
Brenda Wallace <brenda@wallace.net.nz>
2016-12-18 17:59:25 +1300

    Specs for frontpage

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -3,10 +3,28 @@ require 'rails_helper'
 RSpec.describe WelcomeController, type: :controller do
   include Devise::Test::ControllerHelpers
   let(:user) { FactoryGirl.create(:user) }
+  let(:home_type) { FactoryGirl.create(:home_type) }
+  let(:home) { FactoryGirl.create(:home, home_type: home_type) }
+  let(:sensor) { FactoryGirl.create(:sensor, home: home) }
   context 'user is not sign in' do
-    describe 'GET show' do
-      before { get :index }
-      it { expect(response).to have_http_status(:success) }
+    describe 'GET index' do
+      context 'no sensor data' do
+        before { get :index }
+        it { expect(response).to have_http_status(:success) }
+      end
+      context 'lots of sensor data' do
+        let!(:readings) do
+          [
+            FactoryGirl.create(:reading, sensor: sensor),
+            FactoryGirl.create(:reading, sensor: sensor),
+            FactoryGirl.create(:reading, sensor: sensor),
+            FactoryGirl.create(:reading, sensor: sensor),
+            FactoryGirl.create(:reading, sensor: sensor)
+          ]
+        end
+        before { get :index }
+        it { expect(response).to have_http_status(:success) }
+      end
     end
   end
   context 'user is signed in' do
@@ -14,7 +32,7 @@ RSpec.describe WelcomeController, type: :controller do
       sign_in user
     end
 
-    describe 'GET show' do
+    describe 'GET index' do
       before { get :index }
       it { expect(response).to have_http_status(:success) }
     end
--- spec/factories/home_types.rb
+++ spec/factories/home_types.rb
@@ -1,4 +1,5 @@
 FactoryGirl.define do
   factory :home_type do
+    name { Faker::Space.planet }
   end
 end
