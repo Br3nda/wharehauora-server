318155040fb11db89f6631ad30bcd1807f1e03ff
Brenda Wallace <brenda@wallace.net.nz>
2016-12-19 22:32:56 +0000

    Simpler query for medians

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,48 +1,12 @@
 # frozen_string_literal: true
 class WelcomeController < ApplicationController
   def index
-    @public_homes = Home.where(is_public: true).order(:name)
+    skip_policy_scope
     @home_types = HomeType.all.order(:name)
     @room_types = RoomType.all.order(:name)
 
-    set_temperature
-    set_humidity
-    skip_policy_scope
-  end
-
-  private
-
-  def set_temperature
-    @temperature = {}
-    @home_types.each do |home_type|
-      @temperature[home_type.id] = {}
-      @room_types.each do |room_type|
-        @temperature[home_type.id][room_type.id] = median_temperature(home_type, room_type)
-      end
-    end
-  end
-
-  def set_humidity
-    @humidity = {}
-    @home_types.each do |home_type|
-      @humidity[home_type.id] = {}
-      @room_types.each do |room_type|
-        @humidity[home_type.id][room_type.id] = median_humidity(home_type, room_type)
-      end
-    end
-  end
-
-  def median_temperature(home_type, room_type)
-    Reading.joins(:sensor, sensor: :home)
-           .where('readings.created_at >= ?', 1.hour.ago)
-           .where('homes.home_type_id': home_type.id, 'sensors.room_type_id': room_type.id)
-           .temperature.median(:value)
-  end
-
-  def median_humidity(home_type, room_type)
-    Reading.joins(:sensor, sensor: :home)
-           .where('readings.created_at >= ?', 1.hour.ago)
-           .where('homes.home_type_id': home_type.id, 'sensors.room_type_id': room_type.id)
-           .humidity.median(:value)
+    time_frame = 1.hour.ago
+    @temperature = Reading.readings_by_home_and_room(time_frame).temperature.median(:value)
+    @humidity = Reading.readings_by_home_and_room(time_frame).humidity.median(:value)
   end
 end
--- app/models/reading.rb
+++ app/models/reading.rb
@@ -3,4 +3,10 @@ class Reading < ActiveRecord::Base
   delegate :home, :home_id, to: :sensor, allow_nil: false
   scope :temperature, -> { where(sub_type: MySensors::SetReq::V_TEMP) }
   scope :humidity, -> { where(sub_type: MySensors::SetReq::V_HUM) }
+
+  def self.readings_by_home_and_room(created_after)
+    Reading.joins(:sensor, sensor: :home)
+           .where('readings.created_at >= ?', created_after)
+           .group('home_type_id', 'room_type_id')
+  end
 end
