b738d6411f9897acd801ef3b51ca3d955dc1869d
Brenda Wallace <brenda@wallace.net.nz>
2017-02-22 09:26:41 +1300

    Added join through the rooms table

--- app/models/reading.rb
+++ app/models/reading.rb
@@ -5,7 +5,7 @@ class Reading < ActiveRecord::Base
   scope :humidity, -> { where(sub_type: MySensors::SetReq::V_HUM) }
 
   def self.readings_by_home_and_room(created_after)
-    Reading.joins(:sensor, sensor: :home)
+    Reading.joins(:sensor, sensor: :room).joins('INNER JOIN homes ON rooms.home_id=homes.id')
            .where('home_type_id IS NOT NULL AND room_type_id IS NOT NULL')
            .where('readings.created_at >= ?', created_after)
            .group('home_type_id', 'room_type_id')
