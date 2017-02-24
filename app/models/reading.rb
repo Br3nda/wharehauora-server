1337764ac36f630834cfdbf7f7e266a20835a04b
Brenda Wallace <brenda@wallace.net.nz>
2017-02-24 22:15:32 +1300

    Readings join to home table

--- app/models/reading.rb
+++ app/models/reading.rb
@@ -4,6 +4,8 @@ class Reading < ActiveRecord::Base
   scope :temperature, -> { where(sub_type: MySensors::SetReq::V_TEMP) }
   scope :humidity, -> { where(sub_type: MySensors::SetReq::V_HUM) }
 
+  scope :joins_home, -> { joins(:sensor, sensor: [:room, room: :home]) }
+
   def self.readings_by_home_and_room(created_after)
     Reading.joins(:sensor, sensor: :room).joins('INNER JOIN homes ON rooms.home_id=homes.id')
            .where('home_type_id IS NOT NULL AND room_type_id IS NOT NULL')
--- app/policies/reading_policy.rb
+++ app/policies/reading_policy.rb
@@ -1,9 +1,7 @@
 class ReadingPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
-      query = scope.joins('INNER JOIN sensors ON readings.sensor_id = sensors.id')
-                   .joins('INNER JOIN rooms ON sensors.room_id = rooms.id')
-                   .joins('INNER JOIN homes ON rooms.home_id = homes.id')
+      query = scope.joins_home
       if user
         query.where('owner_id = ? OR is_public = true')
       else
