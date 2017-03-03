5dd2ed70edd0516ad4592013cc7bbeb4ae49d6cb
jake <jake@rabidtech.co.nz>
2017-03-03 13:21:18 +1300

    Now, the rake db:seed can be run over and over again without error
    In addition to the seed script not failing when run again, we now have a default user "rabid@example.com" and a password of "password", who owns the default house. Next, fake up some sensor readings

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,21 +5,38 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+ActiveRecord::Base.transaction do
+  hometypes = ['Owner occupier', 'Rental', 'Council', 'State housing']
+  roles = [{ name: 'janitor', friendly_name: 'System janitor' }]
 
-state_house = HomeType.create!(name: 'State housing')
-HomeType.create!(name: 'Council')
-HomeType.create!(name: 'Rental')
-HomeType.create!(name: 'Owner occupier')
+  hometypes.each do |ht|
+    HomeType.create!(name: ht) unless HomeType.find_by(name: ht)
+  end
 
-Role.create!(name: 'janitor', friendly_name: 'System janitor')
+  roles.each do |r|
+    Role.create!(name: r[:name], friendly_name: r[:friendly_name]) unless Role.find_by(name: r[:name])
+  end
 
-if %w(development test staging).include? Rails.env
-  living_room = RoomType.create!(name: 'Living space')
-  bedroom = RoomType.create!(name: 'Sleeping/Bedroom')
+  if %w(development test staging).include? Rails.env
+    roomtypes = ['Living space', 'Sleeping/Bedroom']
+    roomtypes.each do |rt|
+      RoomType.create!(name: rt) unless RoomType.find_by(name: rt)
+    end
 
-  home = Home.create!(name: 'Example home 1', home_type: state_house)
-  Sensor.create!(name: 'living room', home: home, room_type_id: living_room.id)
-  Sensor.create!(name: "parent's room", home: home, room_type_id: bedroom.id)
-  Sensor.create!(name: "eldest child's room", home: home, room_type_id: bedroom.id)
-  Sensor.create!(name: "youngest child's room", home: home, room_type_id: bedroom.id)
+    state_house = HomeType.find_by(name: 'State housing')
+
+    living_room = RoomType.find_by(name: 'Living space')
+    bedroom = RoomType.find_by(name: 'Sleeping/Bedroom')
+
+    User.create!(id: 1, email: 'rabid@example.com', password: 'password', password_confirmation: 'password') unless User.find_by(email: 'rabid@example.com')
+    user = User.find_by(email: 'rabid@example.com')
+
+    Home.create!(name: 'Example home 1', home_type: state_house, owner_id: user.id) unless Home.find_by(name: 'Example home 1')
+    home = Home.find_by(name: 'Example home 1')
+
+    Sensor.create!(name: 'living room', home: home, room_type_id: living_room.id)
+    Sensor.create!(name: "parent's room", home: home, room_type_id: bedroom.id)
+    Sensor.create!(name: "eldest child's room", home: home, room_type_id: bedroom.id)
+    Sensor.create!(name: "youngest child's room", home: home, room_type_id: bedroom.id)
+  end
 end
