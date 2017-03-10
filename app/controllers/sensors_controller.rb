384f5ba215995810f8974a5cc97e3bc9ffb39202
Brenda Wallace <brenda@wallace.net.nz>
2017-03-10 19:23:47 +1300

    Removed extra unused stuff from sensors

--- app/controllers/sensors_controller.rb
+++ app/controllers/sensors_controller.rb
@@ -11,28 +11,10 @@ class SensorsController < ApplicationController
 
   def show
     authorize @sensor
-
-    @daysago = params[:since].to_i.day.ago
-
-    @readings = @sensor.readings
+    @messages = Message.where(sensor: @sensor)
                        .order(created_at: :desc)
                        .paginate(page: params[:page], per_page: 50)
-
-    @readings = getreadings @daysago
-    @temperature = temperature_data @daysago
-    @humidity = humidity_data @daysago
   end
-
-  # def edit
-  #   @room_types = RoomType.all
-  #   authorize @sensor
-  # end
-
-  # def update
-  #   @sensor.update(sensor_params)
-  #   @sensor.save!
-  #   redirect_to home_sensors_path(@sensor.home)
-  # end
 
   private
 
@@ -47,29 +29,7 @@ class SensorsController < ApplicationController
 
   def permitted_sensor_params
     %i(
-      room_name
-      room_type_id
+      room_id
     )
-  end
-
-  def temperature_data(daysago)
-    time_series Reading.temperature, daysago
-  end
-
-  def humidity_data(daysago)
-    time_series Reading.humidity, daysago
-  end
-
-  def getreadings(daysago)
-    @sensor.readings
-           .where(['created_at >= ?', daysago])
-           .order(created_at: :desc)
-           .paginate(page: params[:page], per_page: 50)
-  end
-
-  def time_series(query, daysago = 1.day.ago)
-    policy_scope(query).where(sensor_id: @sensor.id)
-                       .where(['readings.created_at >= ?', daysago])
-                       .pluck("date_trunc('minute', readings.created_at)", :value)
   end
 end
--- spec/controllers/sensors_controller_spec.rb
+++ spec/controllers/sensors_controller_spec.rb
@@ -20,31 +20,8 @@ RSpec.describe SensorsController, type: :controller do
     before { sign_in user }
 
     describe 'GET show' do
-      before { get :show, id: sensor.id }
+      before { get :show, home_id: home.id, id: sensor.id }
       it { expect(response).to have_http_status(:success) }
-    end
-
-    describe '#update' do
-      before { patch :update, id: sensor.to_param, sensor: { room_name: 'Living room' } }
-      it { expect(response).to redirect_to home_sensors_path(home) }
-    end
-  end
-
-  context "Trying to access another users's data" do
-    before { sign_in user }
-    describe "GET edit for someone else's home" do
-      let(:another_user) { FactoryGirl.create(:user) }
-      let(:another_home) { FactoryGirl.create(:home, owner_id: another_user.id) }
-      let(:another_sensor) { FactoryGirl.create(:sensor, home_id: another_home.id) }
-
-      describe '#show' do
-        before { get :show, id: another_sensor.to_param }
-        it { expect(response).to have_http_status(:not_found) }
-      end
-      describe '#edit' do
-        before { get :show, id: another_sensor.to_param }
-        it { expect(response).to have_http_status(:not_found) }
-      end
     end
   end
 end
