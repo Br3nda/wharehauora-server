191ac0d794843328c104edf3a8a8b4c6a86b5253
Brenda Wallace <brenda@wallace.net.nz>
2017-03-11 19:59:21 +1300

    Fixed reading policy

--- app/policies/reading_policy.rb
+++ app/policies/reading_policy.rb
@@ -3,17 +3,10 @@ class ReadingPolicy < ApplicationPolicy
     def resolve
       query = scope.joins_home
       if user
-        query.where('owner_id = ? OR is_public = true')
+        query.where('owner_id = ? OR is_public = true', user.id)
       else
         query.where(is_public: true)
       end
-      query
     end
-  end
-
-  private
-
-  def owned_by_current_user?
-    record.room.home.owner_id == user.id
   end
 end
