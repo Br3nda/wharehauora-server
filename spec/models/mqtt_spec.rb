35d7082c5aabd0c74de402d7d4c2b55069e1267d
Brenda Wallace <brenda@wallace.net.nz>
2017-03-17 04:08:16 +0000

    Start of model specs

--- spec/models/mqtt_spec.rb
+++ spec/models/mqtt_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Mqtt, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  pending 'fetch_mqtt_user_list'
+  pending 'sync_mqtt_users'
+  pending 'provision_mqtt_user(username, password)'
+  pending 'url'
+  pending 'mqtt_api_creds'
+  it 'makes a faraday_conn' do
+    expect(Mqtt.faraday_conn).to be_a(Faraday::Connection)
+  end
 end
