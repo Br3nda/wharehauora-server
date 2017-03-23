7a4db681fb213a554b004e74eb60076b90f9342d
Brenda Wallace <brenda@wallace.net.nz>
2017-03-24 09:29:43 +1300

    More sensor controller specs

--- spec/controllers/sensors_controller_spec.rb
+++ spec/controllers/sensors_controller_spec.rb
@@ -14,14 +14,31 @@ RSpec.describe SensorsController, type: :controller do
       before { get :show, id: sensor.id }
       it { expect(response).to redirect_to(new_user_session_path) }
     end
+    describe 'delete' do
+      before { delete :destroy, id: sensor.id }
+      it { expect(response).to redirect_to(new_user_session_path) }
+    end
   end
 
   context 'user is signed in' do
     before { sign_in user }
 
+    describe 'GET index' do
+      before { get :index, home_id: home.id }
+      it { expect(response).to have_http_status(:success) }
+      it { expect(assigns(:sensors)).to eq([sensor]) }
+      it { expect(assigns(:home)).to eq(home) }
+    end
+
     describe 'GET show' do
       before { get :show, home_id: home.id, id: sensor.id }
       it { expect(response).to have_http_status(:success) }
+      it { expect(assigns(:sensor)).to eq(sensor) }
+    end
+
+    describe 'delete' do
+      before { delete :destroy, id: sensor.id }
+      it { expect(response).to redirect_to(home_sensors_path(sensor.home)) }
     end
   end
 end
