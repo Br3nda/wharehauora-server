c26982163f39a3d25851a57d7dd92ff5a6e8577e
Brenda Wallace <brenda@wallace.net.nz>
2017-03-25 18:38:51 +1300

    Removed unused parts of home_viewers policy

--- app/policies/home_viewer_policy.rb
+++ app/policies/home_viewer_policy.rb
@@ -1,28 +1,4 @@
 class HomeViewerPolicy < ApplicationPolicy
-  def index?
-    owner? || janitor?
-  end
-
-  def new?
-    owner? || janitor?
-  end
-
-  def edit?
-    owner? || janitor?
-  end
-
-  def show?
-    owner? || janitor?
-  end
-
-  def update?
-    owner? || janitor?
-  end
-
-  def destroy?
-    owner? || janitor?
-  end
-
   class Scope < Scope
     def resolve
       scope.all if user
