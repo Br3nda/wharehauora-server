a0236f86cadbca7a9f53573fd20a159875e3af5a
Brenda Wallace <brenda@wallace.net.nz>
2017-04-06 10:43:05 +1200

    readings controller spec when home has readings

--- spec/controllers/readings_controller_spec.rb
+++ spec/controllers/readings_controller_spec.rb
@@ -15,7 +15,21 @@ RSpec.describe ReadingsController, type: :controller do
     before { sign_in user }
     describe 'GET index' do
       before { get :index, valid_params }
-      it { expect(response).to have_http_status(:success) }
+      describe 'no readings yet' do
+        it { expect(response).to have_http_status(:success) }
+      end
+      describe 'has readings' do
+        before do
+          5.times do
+            room = FactoryGirl.create :room, home: home
+            100.times do
+              FactoryGirl.create :reading, room: room, key: 'temperature'
+              FactoryGirl.create :reading, room: room, key: 'humidity'
+            end
+          end
+        end
+        it { expect(response).to have_http_status(:success) }
+      end
     end
   end
 end
