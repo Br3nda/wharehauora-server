0fc345cbdb036ea51b497165abffd1e6bc85bb21
Brenda Wallace <brenda@wallace.net.nz>
2017-04-07 10:50:06 +1200

    better specs of opendata readings

--- spec/controllers/opendata/readings_controller_spec.rb
+++ spec/controllers/opendata/readings_controller_spec.rb
@@ -3,7 +3,9 @@ require 'rails_helper'
 RSpec.describe Opendata::ReadingsController, type: :controller do
   let(:user) { FactoryGirl.create :user }
   let(:home) { FactoryGirl.create :home, owner: user }
-  let(:valid_params) { { home_id: home.id, key: 'temperature', day: '2017-01-01' } }
+  let(:valid_params) { { key: 'temperature', day: day } }
+  let(:day) { '2017-01-01' }
+
   context 'Not signed in' do
     describe 'GET index' do
       before { get :index, valid_params }
@@ -14,21 +16,33 @@ RSpec.describe Opendata::ReadingsController, type: :controller do
   context 'Signed in as home owner' do
     before { sign_in user }
     describe 'GET index' do
-      before { get :index, valid_params }
       describe 'no readings yet' do
         it { expect(response).to have_http_status(:success) }
       end
       describe 'has readings' do
         before do
+          @readings = []
+          @room_types = []
           5.times do
-            room = FactoryGirl.create :room, home: home
-            100.times do
-              FactoryGirl.create :reading, room: room, key: 'temperature'
-              FactoryGirl.create :reading, room: room, key: 'humidity'
+            room_type = FactoryGirl.create :room_type
+            @room_types << room_type
+            room = FactoryGirl.create :room, home: home, room_type: room_type
+            6.times do
+              @readings << FactoryGirl.create(:reading, room: room, key: 'temperature', value: 10, created_at: day)
+              @readings << FactoryGirl.create(:reading, room: room, key: 'humidity', value: 10, created_at: day)
             end
           end
+          get :index, valid_params
         end
         it { expect(response).to have_http_status(:success) }
+        it 'finds readings, organised by room types' do
+          expected_value = [[Time.zone.parse(day), 10.0]]
+
+          expect(assigns(:data).count).to eq(5)
+          @room_types.each do |rt|
+            expect(assigns(:data)).to include('name' => rt.name, 'data' => expected_value)
+          end
+        end
       end
     end
   end
