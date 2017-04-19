9c1112eeaf8520361da6a4cdd5688eb1c5004332
Brenda Wallace <brenda@wallace.net.nz>
2017-04-19 02:49:11 +0000

    Include sensor

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,6 +2,7 @@ class MessagesController < ApplicationController
   def index
     authorize :message
     @messages = policy_scope Message.where(search_params)
+                .includes(:sensor)
                 .order(created_at: :desc)
                 .paginate(page: params[:page])
   end
