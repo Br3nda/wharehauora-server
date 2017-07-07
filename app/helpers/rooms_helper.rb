0f578d8e8a009ea5f907a0bf18098071613d6c8b
Brenda Wallace <brenda@wallace.net.nz>
2017-07-07 18:17:40 +1200

    Show how old the current reading is

--- app/helpers/rooms_helper.rb
+++ app/helpers/rooms_helper.rb
@@ -10,4 +10,9 @@ module RoomsHelper
     return format '%.1f%%', h unless h.nil?
     '??.?'
   end
+
+  def room_reading_time_ago(room, reading_type)
+    ts = room.last_reading_timestamp reading_type
+    "#{time_ago_in_words(ts)} ago" if ts
+  end
 end
