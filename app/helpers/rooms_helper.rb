38a3da567c6ca7ef7a55239be85fd82a8aa4d1a4
Brenda Wallace <brenda@wallace.net.nz>
2017-07-21 21:23:13 +1200

    Removed unused code in RoomsHelper

--- app/helpers/rooms_helper.rb
+++ app/helpers/rooms_helper.rb
@@ -1,18 +1,2 @@
 module RoomsHelper
-  def room_temperature(room)
-    t = room.temperature
-    return format '%.1fC', t unless t.nil?
-    '??.?'
-  end
-
-  def room_humidity(room)
-    h = room.humidity
-    return format '%.1f%%', h unless h.nil?
-    '??.?'
-  end
-
-  def room_reading_time_ago(room, reading_type)
-    ts = room.last_reading_timestamp reading_type
-    "#{time_ago_in_words(ts)} ago" if ts
-  end
 end
