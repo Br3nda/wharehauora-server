94dec0f97a266482db8babb7ec50386e962e10e2
Brenda Wallace <brenda@wallace.net.nz>
2017-07-25 21:10:58 +1200

    Clean up feature specs for users#index

--- spec/features/admin/users_spec.rb
+++ spec/features/admin/users_spec.rb
@@ -1,41 +1,33 @@
 require 'rails_helper'
 
-RSpec.feature 'Widget management', type: :feature do
-  let(:admin_role) { FactoryGirl.create(:role, name: 'janitor') }
-  let!(:admin_user) { FactoryGirl.create(:user, roles: [admin_role]) }
+RSpec.feature 'Users', type: :feature do
+  let!(:admin_user) { FactoryGirl.create(:admin) }
   let!(:user) { FactoryGirl.create :user }
 
-  context 'not signed in' do
-    scenario 'requests user admin page' do
-      visit '/admin/users'
-      expect(page).not_to have_text('Invite new user')
+  context '#index' do
+    shared_examples 'cannot see users' do
+      before { visit '/admin/users' }
+      it { expect(page).not_to have_text(user.email) }
+    end
+
+    context 'not signed in' do
+      include_examples 'cannot see users'
+    end
+
+    context 'Normal user' do
+      background { login_as(user) }
+      include_examples 'cannot see users'
+    end
+
+    context 'Logged in as admin' do
+      background { login_as(admin_user) }
+      before { visit '/admin/users' }
+      it { expect(page).to have_text(admin_user.email) }
+      it { expect(page).to have_text(user.email) }
     end
   end
 
-  context 'Normal user' do
-    background { login_as(user) }
-    scenario 'requests user admin page' do
-      visit '/admin/users'
-      expect(page).not_to have_text('Invite new user')
-    end
-  end
-
-  context 'Logged in as admin' do
-    background { login_as(admin_user) }
-    scenario 'Admin sees users list' do
-      visit '/admin/users'
-      expect(page).to have_text(admin_user.email)
-      expect(page).to have_text(user.email)
-    end
-
-    # scenario 'User creates a new widget' do
-    #   visit '/admin/users/new'
-    #   expect(page).to have_text('Invite new user')
-    #   fill_in 'Name', with: 'Pool room'
-    #   click_button 'Invite'
-
-    #   # expect(page).to have_text('Room type was successfully created.')
-    #   expect(page).to have_text('Pool room')
-    # end
-  end
+  pending '#edit'
+  pending '#update'
+  pending '#destroy'
 end
