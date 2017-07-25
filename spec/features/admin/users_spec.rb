63fc92887f6cda4e8afd3fc9f5bb73a4581480ea
Brenda Wallace <brenda@wallace.net.nz>
2017-07-25 21:29:24 +1200

    Spec for user editing

--- spec/features/admin/users_spec.rb
+++ spec/features/admin/users_spec.rb
@@ -4,30 +4,50 @@ RSpec.feature 'Users', type: :feature do
   let!(:admin_user) { FactoryGirl.create(:admin) }
   let!(:user) { FactoryGirl.create :user }
 
-  context '#index' do
-    shared_examples 'cannot see users' do
+  shared_examples 'cannot #index' do
+    describe '#index' do
       before { visit '/admin/users' }
       it { expect(page).not_to have_text(user.email) }
     end
+  end
 
-    context 'not signed in' do
-      include_examples 'cannot see users'
-    end
+  subject { page }
 
-    context 'Normal user' do
-      background { login_as(user) }
-      include_examples 'cannot see users'
-    end
+  context 'not signed in' do
+    include_examples 'cannot #index'
+  end
 
-    context 'Logged in as admin' do
-      background { login_as(admin_user) }
+  context 'Normal user' do
+    background { login_as(user) }
+    include_examples 'cannot #index'
+  end
+
+  context 'Logged in as admin' do
+    background { login_as(admin_user) }
+    describe '#index' do
       before { visit '/admin/users' }
       it { expect(page).to have_text(admin_user.email) }
       it { expect(page).to have_text(user.email) }
     end
-  end
 
-  pending '#edit'
-  pending '#update'
-  pending '#destroy'
+    describe '#edit' do
+      before do
+        visit '/admin/users'
+        click_link user.email
+      end
+      it { is_expected.to have_text('Editing User') }
+    end
+
+    describe '#update' do
+      before do
+        visit '/admin/users'
+        click_link user.email
+        fill_in :user_email, with: 'hiria@example.com'
+        click_button 'save'
+      end
+      it { is_expected.to have_text 'hiria@example.com' }
+    end
+
+    pending '#destroy'
+  end
 end
