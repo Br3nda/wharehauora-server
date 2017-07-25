1548d26954a2d7c3147df0d61c71054b9b5db396
Brenda Wallace <brenda@wallace.net.nz>
2017-07-25 05:00:34 +0000

    Adding more specs for sensors

--- spec/features/sensors_spec.rb
+++ spec/features/sensors_spec.rb
@@ -2,35 +2,55 @@ require 'rails_helper'
 
 RSpec.feature 'Sensors', type: :feature do
   let(:user) { FactoryGirl.create :user }
-  let(:janitor) { FactoryGirl.create :role, name: 'janitor' }
-  let(:admin_user) { FactoryGirl.create :user, roles: [janitor] }
-
+  let(:admin_user) { FactoryGirl.create :admin }
   let(:home_type) { FactoryGirl.create :home_type }
-  let(:home) { FactoryGirl.create :home, owner_id: user.id, home_type: home_type, name: 'Toku whare whanau' }
+  let(:home) { FactoryGirl.create :home, owner_id: user.id, home_type: home_type, name: 'Taku whare whanau' }
 
   subject { page }
 
-  context 'Normal user' do
+  shared_examples 'then a 1 new sensor is detected' do
+    include_examples 'rooms#index'
+    it { is_expected.to have_text 'new sensors detected' }
+    it { is_expected.to have_link 'Assign to room' }
+    it { is_expected.to have_text sensor.node_id }
+  end
+
+  shared_examples 'no sensors detected' do
+    include_examples 'rooms#index'
+    it { is_expected.not_to have_text 'new sensors detected' }
+    it { is_expected.not_to have_link 'Assign to room' }
+  end
+
+  shared_examples 'rooms#index' do
+    before { visit "/homes/#{home.id}/rooms" }
+  end
+
+  shared_examples 'when home has one sensor' do
+    let!(:sensor) { FactoryGirl.create :sensor, home: home }
+    it { expect(home.sensors.size).to eq(1) }
+  end
+
+  shared_examples 'sensor has messages' do
+    let!(:sensor) { FactoryGirl.create :sensor, home: home }
+    let!(:messages) { FactoryGirl.create :message, 100, sensor: sensor }
+  end
+
+  context 'normal user' do
     background { login_as(home.owner) }
-    # we see a list of sensors on the Homes#index page
     context 'rooms#index' do
       context '1 unassigned sensor' do
-        let!(:sensor) { FactoryGirl.create :sensor, home: home }
-        before { visit "/homes/#{sensor.home_id}/rooms" }
-        it { is_expected.to have_text 'new sensors detected' }
-        it { is_expected.to have_link 'Assign to room' }
-        it { is_expected.to have_text sensor.node_id }
+        include_examples 'when home has one sensor'
+        include_examples 'then a 1 new sensor is detected'
       end
 
       context 'no sensors' do
-        before { visit "/homes/#{home.id}/rooms" }
-        it { is_expected.not_to have_text 'new sensors detected' }
-        it { is_expected.not_to have_link 'Assign to room' }
+        include_examples 'no sensors detected'
       end
     end
 
     context 'assign sensor' do
       let!(:sensor) { FactoryGirl.create :sensor, home: home }
+
       context 'to a new room' do
         before do
           visit "/homes/#{home.id}/rooms"
@@ -39,9 +59,10 @@ RSpec.feature 'Sensors', type: :feature do
           click_button 'Save'
         end
         it { is_expected.to have_text 'room of oarsum' }
-        it { is_expected.not_to have_text 'new sensors detected' }
         it { is_expected.to have_link 'Analyse' }
+        include_examples 'no sensors detected'
       end
+
       context 'home has one room' do
         let!(:existing_room) { FactoryGirl.create :room, name: 'library', home: home }
         before do
@@ -53,6 +74,44 @@ RSpec.feature 'Sensors', type: :feature do
         it { is_expected.not_to have_text 'new sensors detected' }
         it { is_expected.to have_text existing_room.name }
         it { is_expected.to have_link 'Analyse' }
+      end
+    end
+  end
+
+  context 'Not signed in' do
+  end
+
+  context 'Signed in as an admin users' do
+    background { login_as(admin_user) }
+
+    context 'sensors#index' do
+      context '1 unassigned sensor' do
+        include_examples 'when home has one sensor'
+        before { visit "/homes/#{home.id}/sensors" }
+        it { is_expected.to have_text sensor.node_id }
+      end
+    end
+
+    context 'sensors#delete' do
+      context '1 unassigned sensor' do
+        include_examples 'when home has one sensor'
+        include_examples 'sensor has messages'
+        before do
+          visit "/homes/#{home.id}/sensors"
+          click_button 'delete'
+        end
+        it { is_expected.to have_text 'sensor deleted' }
+      end
+    end
+
+    context 'rooms#index' do
+      context '1 unassigned sensor' do
+        include_examples 'when home has one sensor'
+        include_examples 'then a 1 new sensor is detected'
+      end
+
+      context 'no sensors' do
+        include_examples 'no sensors detected'
       end
     end
   end
