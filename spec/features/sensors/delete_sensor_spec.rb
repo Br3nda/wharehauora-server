81184b68781340168eb39b19563881e199650dcd
Brenda Wallace <brenda@wallace.net.nz>
2017-07-30 11:34:41 +1200

    Whare owner can delete sensors

--- spec/features/sensors/delete_sensor_spec.rb
+++ spec/features/sensors/delete_sensor_spec.rb
@@ -12,21 +12,37 @@ RSpec.feature 'admin lists sensors', type: :feature do
 
   subject { page }
 
-  shared_examples 'delete sensor' do
-    describe 'deletes a sensor in home' do
-      before do
-        visit "/homes/#{sensor.home.id}/sensors"
-        # click_link "/sensors/#{sensor.id}" # delete method
-        click_link 'delete'
-      end
-      it { is_expected.not_to have_text sensor.node_id }
+  shared_examples 'sensor was deleted' do
+    it 'sensor was deleted' do
+      is_expected.not_to have_text sensor.node_id
     end
   end
 
-  # context 'signed in as a normal user' do
-  #   background { login_as(home.owner) }
-  #   include_examples 'lists sensors'
-  # end
+  shared_examples 'can delete sensors' do
+    before(:each) do
+      visit "/homes/#{sensor.home.id}/sensors"
+      # click_link "/sensors/#{sensor.id}" # delete method
+      click_link 'delete'
+    end
+    context 'unassigned sensor' do
+      let(:sensor) { FactoryGirl.create :sensor, home: home, room: nil }
+      include_examples 'sensor was deleted'
+    end
+    context 'assigned sensor' do
+      let(:room) { FactoryGirl.create :room, home: home }
+      let(:sensor) { FactoryGirl.create :sensor, home: home, room: room }
+      include_examples 'sensor was deleted'
+    end
+    context 'sensor with messages' do
+      let(:sensor) { FactoryGirl.create :sensor_with_messages, home: home }
+      include_examples 'sensor was deleted'
+    end
+  end
+
+  context 'signed in as a normal user' do
+    background { login_as(home.owner) }
+    include_examples 'can delete sensors'
+  end
 
   # context 'signed in as whanau' do
   #   background { login_as(whanau) }
@@ -35,18 +51,6 @@ RSpec.feature 'admin lists sensors', type: :feature do
 
   context 'signed in as admin' do
     background { login_as(FactoryGirl.create(:admin)) }
-    context 'unassigned sensor' do
-      let(:sensor) { FactoryGirl.create :sensor, home: home, room: nil }
-      include_examples 'delete sensor'
-    end
-    context 'assigned sensor' do
-      let(:room) { FactoryGirl.create :room, home: home }
-      let(:sensor) { FactoryGirl.create :sensor, home: home, room: room }
-      include_examples 'delete sensor'
-    end
-    context 'sensor with messages' do
-      let(:sensor) { FactoryGirl.create :sensor_with_messages, home: home }
-      include_examples 'delete sensor'
-    end
+    include_examples 'can delete sensors'
   end
 end
