bcadf60829cd8bcb9124f9f3af49887a2a2485ed
Josh McArthur <joshua.mcarthur@gmail.com>
2017-08-04 20:56:32 +1200

    Add factory for creating a valid OAuth access token

---
+++ spec/factories/oauth_access_token.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :oauth_access_token, class: Doorkeeper::AccessToken do
+    sequence(:resource_owner_id) { |n| n }
+    association :application, factory: :oauth_application
+    expires_in 2.hours
+  end
+end
+
