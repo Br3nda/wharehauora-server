831f08a4e8bf977c090942d5e8d006c6be51b1dc
Matt Powell <fauxparse@gmail.com>
2017-08-24 09:24:41 +1200

    Add a feature spec for deleting a whānau member (#155)

---
+++ spec/features/whanau_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.feature 'Whānau', type: :feature do
+  let(:user) { FactoryGirl.create :user }
+  let(:friend) { FactoryGirl.create :user }
+  let(:home) { FactoryGirl.create :home, owner_id: user.id }
+
+  context 'With a whānau member' do
+    let!(:viewer) { home.home_viewers.create!(user: friend) }
+
+    background { login_as(user) }
+
+    scenario 'Views their whānau' do
+      visit home_home_viewers_path(home)
+      expect(page).to have_text(friend.email)
+    end
+
+    scenario 'Removes their friend' do
+      visit home_home_viewers_path(home)
+      within(:css, "[data-home-viewer-id=\"#{viewer.id}\"]") do
+        find(:css, 'a[data-method="delete"]').click
+      end
+      expect(page).to have_current_path(home_home_viewers_path(home))
+      expect(page).not_to have_text(friend.email)
+    end
+  end
+end
