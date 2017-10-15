eb25d97c1c5a96996928664cbd9fd1e8ecdaa1e1
Brenda Wallace <brenda@wallace.net.nz>
2017-10-15 18:25:53 +1300

    authorise by door keeper if the caller sends a token

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -4,15 +4,15 @@ class Api::V1::BaseController < JSONAPI::ResourceController
 
   private
 
+  # def current_resource_owner
+  #   User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
+  # end
+
+  # def pundit_user
+  #   current_resource_owner
+  # end
+
   def auth!
-    doorkeeper_authorize! if current_user.blank?
-  end
-
-  def current_resource_owner
-    User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
-  end
-
-  def pundit_user
-    current_resource_owner
+    before_action :doorkeeper_authorize! if doorkeeper_token
   end
 end
