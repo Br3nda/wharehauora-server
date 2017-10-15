f740c4f01a2053b0018ff82de8328f22add93f4c
Brenda Wallace <brenda@wallace.net.nz>
2017-10-15 20:38:19 +1300

    Find user from oauth token

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -4,13 +4,13 @@ class Api::V1::BaseController < JSONAPI::ResourceController
 
   private
 
-  # def current_resource_owner
-  #   User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
-  # end
+  def current_resource_owner
+    User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
+  end
 
-  # def pundit_user
-  #   current_resource_owner
-  # end
+  def current_user
+    current_resource_owner
+  end
 
   def auth!
     doorkeeper_authorize! if doorkeeper_token
