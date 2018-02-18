191140edb61ef03dfef95be6d6b13732e1ab6c97
Brenda Wallace <brenda@wallace.net.nz>
2018-02-18 14:19:52 +1300

    spec change: Only look for room data in the page (not the menu)

--- spec/features/rooms/rooms_index_spec.rb
+++ spec/features/rooms/rooms_index_spec.rb
@@ -35,6 +35,7 @@ RSpec.feature 'Rooms', type: :feature do
 
       describe ' visit #index' do
         shared_examples 'show home and room' do
+          subject { find('.summary-grid') }
           it { is_expected.to have_text(home.name, maximum: 1) }
           it { is_expected.to have_text(room.name, maximum: 1) }
           it { is_expected.to have_text(room.room_type.name) }
