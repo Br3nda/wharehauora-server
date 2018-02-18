5d780586954ef43916718f299b39d57902d56b06
Brenda Wallace <brenda@wallace.net.nz>
2018-02-18 17:43:21 +1300

    Classes on readings of rooms#show

--- app/helpers/rooms_helper.rb
+++ app/helpers/rooms_helper.rb
@@ -11,6 +11,21 @@ module RoomsHelper
     display_metric room, 'dewpoint'
   end
 
+  TEMPERATURE = { hot: '', ok: 'temp-mid-a', cold: 'temp-low-1a' }.freeze
+  HUMIDTY = { wet: 'hum-high-2a', damp: 'hum-high-2a', ok: 'hum-mid-a' }.freeze
+
+  def temperature_class(room)
+    return TEMPERATURE[:cold] if room.too_cold?
+    return TEMPERATURE[:hot] if room.too_hot?
+    TEMPERATURE[:ok]
+  end
+
+  def humidity_class(room)
+    return HUMIDTY[:wet] if room.below_dewpoint?
+    return HUMIDTY[:damp] if room.near_dewpoint?
+    HUMIDTY[:ok]
+  end
+
   private
 
   def display_metric(room, key)
