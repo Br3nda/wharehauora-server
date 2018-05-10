e0254dd1b9136c909cdf0eee058c9927fea815b3
Brenda Wallace <shiny@cpan.org>
2018-05-10 14:26:24 +1200

    Simpler readings api response

--- app/controllers/api/v1/readings_controller.rb
+++ app/controllers/api/v1/readings_controller.rb
@@ -1,2 +1,19 @@
-class Api::V1::ReadingsController < Api::V1::BaseController
+class Api::V1::ReadingsController < ApplicationController
+  respond_to :json
+
+  def index
+    @room = Room.find(params[:room_id])
+    authorize @room, :show?
+    @readings = {}
+    %w[temperature humidity dewpoint].each do |key|
+      @readings[key] = readings(params[:week], key)
+    end
+    render json: { room: @room, readings: @readings }
+  end
+
+  def readings(week_start, key)
+    policy_scope(Reading.where(key: key, room: @room).by_week(week_start))
+      .group("date_trunc('hour', readings.created_at)")
+      .median(:value)
+  end
 end
