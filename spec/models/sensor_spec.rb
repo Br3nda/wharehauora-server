e0dc43a496bffd3dfa0e53a177733e01991e1c31
Brenda Wallace <shiny@cpan.org>
2018-07-02 16:04:05 +1200

    SEnsor mac addresses are unique too

--- spec/models/sensor_spec.rb
+++ spec/models/sensor_spec.rb
@@ -1,4 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Sensor, type: :model do
+  describe "sensor mac address is unique" do
+    it do
+      FactoryBot.create :sensor, mac_address: 'xyz'
+      expect do
+        FactoryBot.create :sensor, mac_address: 'xyz'
+      end.to raise_error ActiveRecord::RecordInvalid
+    end
+  end
 end
