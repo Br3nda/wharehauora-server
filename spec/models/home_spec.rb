afc395b3cf64e0626c8fd0a4bae7b95ff6f5d3f5
Brenda Wallace <shiny@cpan.org>
2018-07-03 07:51:58 +1200

    Tests for mqtt user for a home

--- spec/models/home_spec.rb
+++ spec/models/home_spec.rb
@@ -9,4 +9,10 @@ RSpec.describe Home, type: :model do
       end.to raise_error ActiveRecord::RecordInvalid
     end
   end
+  describe 'provisions user' do
+    let(:home) { FactoryBot.create :home, gateway_mac_address: 'abc' }
+    before { home.provision_mqtt! }
+    it { expect(home.mqtt_user.username).to eq home.gateway_mac_address }
+    it { expect(home.mqtt_user.password).to eq 'df45eeca8914b6680cebe6b41edc65a7' }
+  end
 end
