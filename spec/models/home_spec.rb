48fba08ed31abc7be7c7e9992806523fb637aa21
Brenda Wallace <shiny@cpan.org>
2018-07-02 20:18:31 +1200

    Style fix ups

--- spec/models/home_spec.rb
+++ spec/models/home_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Home, type: :model do
-  describe "gateway_mac_address is unique" do
+  describe 'gateway_mac_address is unique' do
     it do
-      home = FactoryBot.create :home, gateway_mac_address: 'abc'
+      FactoryBot.create :home, gateway_mac_address: 'abc'
       expect do
         FactoryBot.create :home, gateway_mac_address: 'abc'
       end.to raise_error ActiveRecord::RecordInvalid
