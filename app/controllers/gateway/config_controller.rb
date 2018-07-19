20c90177988d6b0ef6c50d4ad09276472c6aa693
Brenda Wallace <shiny@cpan.org>
2018-07-20 08:52:25 +1200

    Firmware can't do SSL yet

--- app/controllers/gateway/config_controller.rb
+++ app/controllers/gateway/config_controller.rb
@@ -2,7 +2,7 @@ class Gateway::ConfigController < ApplicationController
   def show
     skip_authorization
     @server = Mqtt.mqtt_api_creds.hostname
-    @port = ENV['MQTT_SSL_PORT']
+    @port = Mqtt.mqtt_api_creds.port
     render inline: "#{@server}:#{@port}"
   end
 end
