91d43cc891df17e876a9c9032c298bcaee604c4a
Awesome Code <team@xinminlabs.com>
2018-07-27 19:42:49 +0000

    Auto corrected by following Javascript no-var

--- app/assets/javascripts/rooms.js
+++ app/assets/javascripts/rooms.js
@@ -10,7 +10,7 @@ function setupRoomDataReloader(room_id) {
 
 
 function getRoomData(room_id) {
-  var url = '/api/v1/rooms/' + room_id + '.json';
+  let url = '/api/v1/rooms/' + room_id + '.json';
   $.get(url)
     .done((response) => {
       updateRoomDisplay(room_id, response.data);
@@ -22,21 +22,21 @@ function getRoomData(room_id) {
 
 
 function displayErrorForRoom(room_id, data) {
-  var keys = ['temperature', 'humidity', 'dewpoint'];
+  let keys = ['temperature', 'humidity', 'dewpoint'];
   keys.forEach((key, index) => {
-    var div = '#room-' + room_id + '-' + key + '-';
+    let div = '#room-' + room_id + '-' + key + '-';
     $(div + 'value').text('ERROR');
   });
 }
 
 function updateRoomDisplay(room_id, data) {
-  var keys = ['temperature', 'humidity', 'dewpoint'];
+  let keys = ['temperature', 'humidity', 'dewpoint'];
 
-  var readings = data.attributes.readings;
+  let readings = data.attributes.readings;
 
   keys.forEach((key, index) => {
-    var reading = readings[key];
-    var div = '#room-' + room_id + '-' + key + '-';
+    let reading = readings[key];
+    let div = '#room-' + room_id + '-' + key + '-';
     if (reading) {
 
       // Show the value
@@ -47,7 +47,7 @@ function updateRoomDisplay(room_id, data) {
       jQuery(div + 'timestamp').timeago();
 
       // Mark whether this reading is current or old
-      var current_reading_div = $('#room-' + room_id + '-no-' + key);
+      let current_reading_div = $('#room-' + room_id + '-no-' + key);
       if (reading.current) {current_reading_div.hide();}
       else {current_reading_div.show();}
     } else {
@@ -57,24 +57,24 @@ function updateRoomDisplay(room_id, data) {
   });
 
   // Sets the class on the room card, blue/green
-  var conditions_table = $('#room-' + room_id + '-table');
+  let conditions_table = $('#room-' + room_id + '-table');
   if (data.attributes.ratings.good) {
     conditions_table.addClass('conditions-table-good').removeClass('conditions-table-bad');
   } else {
     conditions_table.addClass('conditions-table-bad').removeClass('conditions-table-good');
   }
 
-  var div = '#room-' + room_id + '-';
+  let div = '#room-' + room_id + '-';
 
-  var too_cold_div = $(div + 'too-cold');
+  let too_cold_div = $(div + 'too-cold');
   if (data.attributes.ratings.too_cold) {too_cold_div.show();}
   else {too_cold_div.hide();}
 
-  var too_hot_div = $(div + 'too-hot');
+  let too_hot_div = $(div + 'too-hot');
   if (data.attributes.ratings.too_hot) {too_hot_div.show();}
   else {too_hot_div.hide();}
 
-  var no_sensors_div = $(div + 'no-sensors');
+  let no_sensors_div = $(div + 'no-sensors');
   if (data.attributes.sensor_count === 0) {no_sensors_div.show();}
   else {no_sensors_div.hide();}
 
