3ad7f7fd9eea78906ba78d58716950e0c3b3c72a
Awesome Code <team@xinminlabs.com>
2018-07-27 19:32:58 +0000

    Auto corrected by following Javascript multiline-comment-style

--- app/assets/javascripts/rooms.js
+++ app/assets/javascripts/rooms.js
@@ -1,5 +1,7 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+/*
+ * Place all the behaviors and hooks related to the matching controller here.
+ * All this logic will automatically be available in application.js.
+ */
 
 function setupRoomDataReloader(room_id) {
   $('.room-' + room_id + '-list').hide();
