2a8b9621cf053523a5724bdedce649cdd7e154ee
Brenda Wallace <shiny@cpan.org>
2018-08-03 11:11:46 +1200

    Revert "Auto corrected by following Javascript keyword-spacing"
    This reverts commit 11667f23df5715a8b22877065d0827a55ec81a66.

--- app/assets/javascripts/rooms.js
+++ app/assets/javascripts/rooms.js
@@ -46,7 +46,7 @@ function updateRoomDisplay(room_id, data) {
 
       // Mark whether this reading is current or old
       var current_reading_div = $('#room-' + room_id + '-no-' + key);
-      if (reading.current) current_reading_div.hide();
+      if(reading.current) current_reading_div.hide();
       else current_reading_div.show();
     } else {
       $(div + 'value').text('??');
@@ -56,7 +56,7 @@ function updateRoomDisplay(room_id, data) {
 
   // Sets the class on the room card, blue/green
   var conditions_table = $('#room-' + room_id + '-table');
-  if (data.attributes.ratings.good) {
+  if(data.attributes.ratings.good) {
     conditions_table.addClass('conditions-table-good').removeClass('conditions-table-bad');
   } else {
     conditions_table.addClass('conditions-table-bad').removeClass('conditions-table-good');
@@ -65,15 +65,15 @@ function updateRoomDisplay(room_id, data) {
   var div = '#room-' + room_id + '-';
 
   var too_cold_div = $(div + 'too-cold');
-  if (data.attributes.ratings.too_cold) too_cold_div.show();
+  if(data.attributes.ratings.too_cold) too_cold_div.show();
   else too_cold_div.hide();
 
   var too_hot_div = $(div + 'too-hot');
-  if (data.attributes.ratings.too_hot) too_hot_div.show();
+  if(data.attributes.ratings.too_hot) too_hot_div.show();
   else too_hot_div.hide();
 
   var no_sensors_div = $(div + 'no-sensors');
-  if (data.attributes.sensor_count === 0) no_sensors_div.show();
+  if(data.attributes.sensor_count === 0) no_sensors_div.show();
   else no_sensors_div.hide();
 
   $('.room-' + room_id + '-list').show();
