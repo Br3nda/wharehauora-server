a882afe33eda7e17eab1b5592505f278ec8e4f3c
Brenda Wallace <shiny@cpan.org>
2018-08-03 11:11:37 +1200

    evert "Auto corrected by following Javascript multiline-comment-style"
    This reverts commit 3ad7f7fd9eea78906ba78d58716950e0c3b3c72a.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,20 +1,18 @@
-/*
- * This is a manifest file that'll be compiled into application.js, which will include all the files
- * Listed below.
- *
- * Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
- * Or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
- *
- * It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
- * Compiled file.
- *
- * Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
- * About supported directives.
- *
- *= require jquery
- *= require jquery_ujs
- *= require rails-timeago
- *= require chartkick
- *= require_tree .
- *= require moment
- */
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// Listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// Or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// Compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// About supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require rails-timeago
+//= require chartkick
+//= require_tree .
+//= require moment
