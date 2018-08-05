099acee472a9310ff3eef766cf6c6fa43285c6a0
Awesome Code <team@xinminlabs.com>
2018-08-05 03:26:18 +0000

    Auto corrected by following Ruby Style/BracesAroundHashParameters

--- spec/controllers/homes_controller_spec.rb
+++ spec/controllers/homes_controller_spec.rb
@@ -89,7 +89,7 @@ RSpec.describe HomesController, type: :controller do
       subject { assigns(:home) }
 
       describe 'Creating a home' do
-        before { put :create, { home: { name: 'My new home' } } }
+        before { put :create, home: { name: 'My new home' } }
 
         it { expect(subject.name).to eq 'My new home' }
         it { expect(subject.owner).to eq user }
