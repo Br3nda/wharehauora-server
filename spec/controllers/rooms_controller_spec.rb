6da4f6b15988bff7054187f4d539f91427bf1229
Awesome Code <team@xinminlabs.com>
2018-09-16 11:27:49 +0000

    Auto corrected by following Ruby Layout/AlignHash

--- spec/controllers/rooms_controller_spec.rb
+++ spec/controllers/rooms_controller_spec.rb
@@ -62,7 +62,7 @@ RSpec.describe RoomsController, type: :controller do
         before do
           patch :update, params: {
             home_id: room.home.id, id: room.to_param,
-                                   room: { name: 'Living room' } }
+            room: { name: 'Living room' } }
         end
 
         it { expect(response).to redirect_to home_rooms_path(home) }
@@ -87,7 +87,7 @@ RSpec.describe RoomsController, type: :controller do
         before do
           patch :update, params: {
             home_id: room.home.id, id: room.to_param,
-                                   room: { name: 'Living room' } }
+            room: { name: 'Living room' } }
         end
 
         it { expect(response).to have_http_status(:redirect) }
