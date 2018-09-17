51185cb91fad512dc4694fc27e8b7c6e175f18e8
Brenda Wallace <brenda@wallace.net.nz>
2018-09-17 22:37:58 +1200

    Change class on dashboard based on conditions

--- app/helpers/rooms_helper.rb
+++ app/helpers/rooms_helper.rb
@@ -13,6 +13,26 @@ module RoomsHelper
     display_metric room, 'dewpoint'
   end
 
+  def temperature_reading_class(room)
+    if room.too_cold?
+      'temp-low-2a'
+    elsif room.too_hot?
+      'temp-high-1a'
+    else
+      'temp-mid-a'
+    end
+  end
+
+  def humidity_reading_class(room)
+    if room.below_dewpoint?
+      'hum-high-2a'
+    elsif room.near_dewpoint?
+      'hum-high-2b'
+    else
+      'hum-mid-a'
+    end
+  end
+
   private
 
   def display_metric(room, key)
