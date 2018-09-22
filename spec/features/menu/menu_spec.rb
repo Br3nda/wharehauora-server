6510780a18670fde3f130c27c0e5618d8dbbc800
Brenda Wallace <brenda@wallace.net.nz>
2018-09-22 15:56:54 +1200

    Updated menu specs to test the home_path

--- spec/features/menu/menu_spec.rb
+++ spec/features/menu/menu_spec.rb
@@ -20,17 +20,27 @@ RSpec.describe 'Homes', type: :feature do
     it { is_expected.not_to have_link('Logout') }
   end
 
-  context 'Normal user' do
-    before do
-      login_as(user)
-      visit '/'
-    end
-
-    it { is_expected.to have_text(home.name) }
-    it { is_expected.to have_link(room.name, href: room_path(room)) }
-    it { is_expected.to have_link('Whānau') }
+  shared_examples 'password and logout links' do
     it { is_expected.to have_link('Change password') }
     it { is_expected.to have_link('Logout') }
+  end
+
+  context 'Normal user' do
+    before { login_as(user) }
+
+    describe 'root path' do
+      before { visit '/' }
+      it { is_expected.to have_link(home.name) }
+      include_examples 'password and logout links'
+    end
+
+    describe 'selected a home' do
+      before { visit home_path(home) }
+      it { is_expected.to have_text(home.name) }
+      it { is_expected.to have_link(room.name, href: room_path(room)) }
+      it { is_expected.to have_link('Whānau') }
+      include_examples 'password and logout links'
+    end
   end
 
   context 'Admin users' do
@@ -39,13 +49,14 @@ RSpec.describe 'Homes', type: :feature do
       visit '/'
     end
 
+    it { is_expected.to have_link(home.name) }
+
     it { is_expected.to have_link('Whare') }
     it { is_expected.to have_link('Whare Types') }
     it { is_expected.to have_link('Room Types') }
     it { is_expected.to have_link('Users') }
     it { is_expected.to have_link('MQTT') }
     it { is_expected.to have_link('Messages') }
-    it { is_expected.to have_link('Change password') }
-    it { is_expected.to have_link('Logout') }
+    include_examples 'password and logout links'
   end
 end
