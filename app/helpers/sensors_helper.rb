1be90d913832392cfdf6b1924876d779237c76ce
Brenda Wallace <brenda@wallace.net.nz>
2018-09-22 13:38:16 +1200

    Fix up display of sensor status in room#show

--- app/helpers/sensors_helper.rb
+++ app/helpers/sensors_helper.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 module SensorsHelper
+  def sensor_last_message(sensor)
+    return '' unless sensor.messages.size.positive?
+    "#{time_ago_in_words sensor.last_message} ago"
+  end
+
+  def sensor_first_detected(sensor)
+    sensor.created_at.localtime
+  end
 end
