22c48e709170cdcfce1a6b00eaf6de8af6b95d95
Brenda Wallace <brenda@wallace.net.nz>
2018-09-22 14:36:24 +1200

    Removed sensors#index and #sensors#delete

--- app/controllers/sensors_controller.rb
+++ app/controllers/sensors_controller.rb
@@ -5,20 +5,6 @@ class SensorsController < ApplicationController
   before_action :set_sensor, only: %i[show edit destroy update]
   respond_to :html
 
-  def index
-    @home = policy_scope(Home).find(params[:home_id])
-    authorize @home
-    @sensors = @home.sensors
-                    .includes(:room)
-                    .order(:node_id)
-                    .paginate(page: params[:page])
-  end
-
-  def destroy
-    @sensor.destroy!
-    redirect_to home_sensors_path(@sensor.home)
-  end
-
   def show
     respond_with(@sensor)
   end
