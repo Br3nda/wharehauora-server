9a5b40f148d8119c18a5a00454144d1e1ce3abb9
Brenda Wallace <brenda@wallace.net.nz>
2018-09-24 19:19:24 +1200

    Updating homes specs

--- spec/features/admin/home_types_spec.rb
+++ spec/features/admin/home_types_spec.rb
@@ -28,7 +28,7 @@ RSpec.describe 'Whare Types', type: :feature do
 
     before { login_as(admin_user) }
 
-    it 'Admin sees home_types list' do
+    scenario 'Admin sees home_types list' do
       visit '/admin/home_types'
       expect(page).to have_text('Cake whare')
     end
@@ -40,7 +40,7 @@ RSpec.describe 'Whare Types', type: :feature do
       expect(page).to have_text('Marae')
     end
 
-    it 'deletes a home_type' do
+    scenario 'deletes a home_type' do
       visit admin_home_types_path
       is_expected.to have_text existing.name
       click_link(alt: 'remove')
